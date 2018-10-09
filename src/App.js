2a3dc9d72be2cc1a655775fbffb486db34df8f4f
feelextra <felxtra@gmail.com>
2018-10-09 14:49:11 +0300

    Initialized repository

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'; // new
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state = {
+        todos: []
+    };
+
+    // new
+    componentDidMount() {
+        this.getTodos();
+    }
+
+    // new
+    getTodos() {
+        axios
+            .get('http://localhost:8000/api/')
+            .then(res => {
+                this.setState({ todos: res.data });
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
+    render() {
+      return (
+        <div>
+          { this.state.todos.map(item => (
+              <div key={item.id}>
+                  <h1>{item.title}</h1>
+                  <span>{item.body}</span>
+              </div>
+          ))}
+        </div>
+      );
+    }
 }
 
 export default App;
