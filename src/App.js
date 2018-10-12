a2ab0dcc559dfb2c6962adf94346b32d867fe9a5
feelextra <felxtra@gmail.com>
2018-10-12 22:23:43 +0300

    added functional programming usages, removed data structure manipulation

--- src/App.js
+++ src/App.js
@@ -26,10 +26,10 @@ class App extends Component {
             .post(this.API_URL, params)
             .then(res => {
                 if (res.status === 201){
-                    const todoObj = res.data; // { 'id':..., 'title':..., 'completed':... }
+                    const todo = res.data; // { 'id':..., 'title':..., 'completed':... }
                     const todos = this.state.todos;
-                    const updatedTodos = this.insertTodoObj(todoObj, todos);
-                    this.setState({ todos: updatedTodos });
+                    const newTodos = todos.concat([todo]);
+                    this.setState({ todos: newTodos });
                 }
             })
             .catch(err => {
@@ -42,14 +42,8 @@ class App extends Component {
         axios
             .get(this.API_URL)
             .then(res => {
-                const arrTodos = res.data; // [ { 'id':..., 'title':..., 'completed':... }, {...}, ... ]
-                let objTodos = {}; // { <id_x>: {'title':..., 'completed':...}, <id_y>:{...}, ... }
-                for (let i=0; i<arrTodos.length; i++) {
-                    const todoObj = arrTodos[i];  // { 'id':..., 'title':..., 'completed':... }
-                    objTodos = this.insertTodoObj(todoObj, objTodos);
-                }
-                const updatedObjTodos = objTodos;
-                this.setState( { todos: updatedObjTodos })
+                const todos = res.data; // [ { 'id':..., 'title':..., 'completed':... }, {...}, ... ]
+                this.setState( { todos: todos });
             })
             .catch(err => {
                 console.log(err);
@@ -64,9 +58,17 @@ class App extends Component {
         .then(res => {
             if (res.status === 200) {
                 const todos = this.state.todos;
-                todos[itemId].title = this.state.inputUpdateTodoValue;
-                const updatedTodos = todos;
-                this.setState({ todos: updatedTodos });
+                const newTodos = todos.map(todo => {
+                    if (todo.id === itemId) {
+                        return {
+                            'id':todo.id,
+                            'title': this.state.inputUpdateTodoValue,
+                            'completed': todo.completed
+                        };
+                    }
+                    return todo;
+                });
+                this.setState({ todos: newTodos });
             }
         })
         .catch(err => {
@@ -80,23 +82,13 @@ class App extends Component {
         .then(res => {
             if (res.status === 204) {
                 const todos = this.state.todos;
-                delete todos[itemId];
-                const updatedTodos = todos;
-                this.setState({ todos: updatedTodos });
+                const newTodos = todos.filter(todo => todo.id !== itemId);
+                this.setState({ todos: newTodos });
             }
         })
         .catch(err => {
             console.log(err);
         });
-    }
-
-    /* Helper method which enables code reuse and separation of concerns */
-    insertTodoObj(todoObj, todos) {
-        todos[todoObj.id] = {
-            'title': todoObj.title,
-            'completed': todoObj.completed
-        };
-        return todos;
     }
 
     updateInputCreateTodoValue(event) {
@@ -123,13 +115,19 @@ class App extends Component {
                     type='text'
                     placeholder='Edited value for todo'>
                 </input>
-                <div> {
-                    Object.keys(this.state.todos).map( todoId => (
-                        <div key={ todoId }>
-                            <span>{this.state.todos[todoId].completed.toString()+' '}</span>
-                            <label>{this.state.todos[todoId].title+' '}</label>
-                            <button onClick={this.updateTodo.bind(this, todoId)}>{'Update'}</button>
-                            <button onClick={this.deleteTodo.bind(this, todoId)}>{'X'}</button>
+                <div> { // Todo Items
+                    this.state.todos.map( todo => (
+                        <div key={ todo.id }>
+                            <span>{todo.completed.toString()+' '}</span>
+                            <label>{todo.title+' '}</label>
+                            <button
+                                onClick={this.updateTodo.bind(this, todo.id)}>
+                                {'Update'}
+                            </button>
+                            <button
+                                onClick={this.deleteTodo.bind(this, todo.id)}>
+                                {'X'}
+                            </button>
                         </div>
                     ))
                 }
