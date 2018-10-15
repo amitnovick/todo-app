3d4a52bb39c25e61ca3fc61ebc40f0ce5c1ca1bb
feelextra <felxtra@gmail.com>
2018-10-15 16:39:30 +0300

    Refactored React component into a dedicated class component: TodoTable

--- src/App.js
+++ src/App.js
@@ -1,140 +1,167 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import {
+  API_URL,
+  SLASH,
+  TODO_TITLE,
+  TODO_ID,
+  TODO_COMPLETED,
+} from './constants/index.js';
 
 class App extends Component {
     constructor(props) {
-        super(props);
-        this.state = {
-            todos: [],
-            inputCreateTodoValue: '',
-            inputUpdateTodoValue: '', // only needed for debugging API interop
-        };
-        this.API_URL = 'http://localhost:8000/api/v1/todos/';
-        this.SLASH = '/';
+      super(props);
+
+      this.state = {
+        todos: [],
+        inputCreateTodoValue: '',
+        inputUpdateTodoValue: '', // only needed for debugging API interop
+      };
+
+      this.createTodo = this.createTodo.bind(this);
+      this.updateInputCreateTodoValue = this.updateInputCreateTodoValue.bind(this);
+      this.updateInputUpdateTodoValue = this.updateInputUpdateTodoValue.bind(this);
     }
  
     componentDidMount() {
-        this.readTodos();
+      this.readTodos();
     }
 
     createTodo() {
-        if (this.state.inputCreateTodoValue.length > 0) {
-            const params = new URLSearchParams();
-            params.append('title', this.state.inputCreateTodoValue);
-            params.append('completed', 'false');
-            axios
-            .post(this.API_URL, params)
-            .then(res => {
-                if (res.status === 201){
-                    const todo = res.data; // { 'id':..., 'title':..., 'completed':... }
-                    const todos = this.state.todos;
-                    const newTodos = todos.concat([todo]);
-                    this.setState({ todos: newTodos });
-                }
-            })
-            .catch(err => {
-                console.log(err);
-            })
-        }
+      if (this.state.inputCreateTodoValue.length > 0) {
+        const params = new URLSearchParams();
+        params.append([TODO_TITLE], this.state.inputCreateTodoValue);
+        params.append([TODO_COMPLETED], 'false');
+
+        axios
+          .post(API_URL, params)
+          .then(res => {
+            if (res.status === 201){
+              const todo = res.data; // { 'id':..., 'title':..., 'completed':... }
+              const newTodos = this.state.todos.concat([todo]);
+              this.setState({ todos: newTodos });
+            }
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      }
     }
 
     readTodos() {
-        axios
-            .get(this.API_URL)
-            .then(res => {
-                const todos = res.data; // [ { 'id':..., 'title':..., 'completed':... }, {...}, ... ]
-                this.setState( { todos: todos });
-            })
-            .catch(err => {
-                console.log(err);
-            });
+      axios
+        .get(API_URL)
+        .then(res => {
+          const todos = res.data; // [ { 'id':..., 'title':..., 'completed':... }, {...}, ... ]
+          this.setState( { todos: todos });
+        })
+        .catch(err => {
+          console.log(err);
+        });
     }
 
     updateTodo(itemId) {
-        const params = new URLSearchParams();
-        params.append('title', this.state.inputUpdateTodoValue);
-        axios
-        .put(this.API_URL + itemId.toString() + this.SLASH, params)
+      const params = new URLSearchParams();
+      params.append([TODO_TITLE], this.state.inputUpdateTodoValue);
+
+      axios
+        .put(API_URL + itemId.toString() + SLASH, params)
         .then(res => {
-            if (res.status === 200) {
-                const todos = this.state.todos;
-                const newTodos = todos.map(todo => {
-                    if (todo.id === itemId) {
-                        return {
-                            'id':todo.id,
-                            'title': this.state.inputUpdateTodoValue,
-                            'completed': todo.completed
-                        };
-                    }
-                    return todo;
-                });
-                this.setState({ todos: newTodos });
+          if (res.status === 200) {
+            const replaceTodoById = (todo) => {
+              if (todo.id === itemId) {
+                return {
+                  [TODO_ID]: todo.id,
+                  [TODO_TITLE]: this.state.inputUpdateTodoValue,
+                  [TODO_COMPLETED]: todo.completed,
+                };
+              }
+              return todo;
             }
+            const newTodos = this.state.todos.map(replaceTodoById);
+            this.setState({ todos: newTodos });
+          }
         })
         .catch(err => {
-            console.log(err);
+          console.log(err);
         });
     }
 
     deleteTodo(itemId) {
-        axios
-        .delete(this.API_URL + itemId.toString() + this.SLASH)
+      axios
+        .delete(API_URL + itemId.toString() + SLASH)
         .then(res => {
-            if (res.status === 204) {
-                const todos = this.state.todos;
-                const newTodos = todos.filter(todo => todo.id !== itemId);
-                this.setState({ todos: newTodos });
-            }
+          if (res.status === 204) {
+            const isNotId = (todo) => todo.id !== itemId;
+            const newTodos = this.state.todos.filter(isNotId);
+            this.setState({ todos: newTodos });
+          }
         })
         .catch(err => {
-            console.log(err);
+          console.log(err);
         });
     }
 
     updateInputCreateTodoValue(event) {
-        this.setState({ inputCreateTodoValue: event.target.value });
+      this.setState({ inputCreateTodoValue: event.target.value });
     }
 
     updateInputUpdateTodoValue(event) {
-        this.setState({ inputUpdateTodoValue: event.target.value });
+      this.setState({ inputUpdateTodoValue: event.target.value });
     }
 
     render() {
-        return (
-            <div>
-                <button onClick={this.createTodo.bind(this)}>{'+'}</button>
-                <input
-                    value={this.state.inputCreateTodoValue}
-                    onChange={event => this.updateInputCreateTodoValue(event)}
-                    type='text'
-                    placeholder='Enter your task here...'>
-                </input>
-                <input
-                    value={this.state.inputUpdateTodoValue}
-                    onChange={event => this.updateInputUpdateTodoValue(event)}
-                    type='text'
-                    placeholder='Edited value for todo'>
-                </input>
-                <div> { // Todo Items
-                    this.state.todos.map( todo => (
-                        <div key={ todo.id }>
-                            <span>{todo.completed.toString()+' '}</span>
-                            <label>{todo.title+' '}</label>
-                            <button
-                                onClick={this.updateTodo.bind(this, todo.id)}>
-                                {'Update'}
-                            </button>
-                            <button
-                                onClick={this.deleteTodo.bind(this, todo.id)}>
-                                {'X'}
-                            </button>
-                        </div>
-                    ))
-                }
-                </div>
-            </div>
-        );
+      const { todos, inputCreateTodoValue, inputUpdateTodoValue } = this.state;
+      return (
+        <div>
+          <button onClick={this.createTodo}>
+            {'+'}
+          </button>
+          <input
+            value={inputCreateTodoValue}
+            onChange={this.updateInputCreateTodoValue}
+            type='text'
+            placeholder='Enter your task here...'
+          />
+          <input
+            value={inputUpdateTodoValue}
+            onChange={this.updateInputUpdateTodoValue}
+            type='text'
+            placeholder='Edited value for todo'
+          />
+          <TodoTable 
+            todosList={todos}
+            updateTodo={this.updateTodo}
+            deleteTodo={this.deleteTodo}
+            that={this}
+          />
+        </div>
+      );
     }
+}
+
+class TodoTable extends Component {
+  render() {
+    const { todosList, updateTodo, deleteTodo, that } = this.props;
+    return (
+      <div>
+      {
+        todosList.map( todo => 
+          <div key={todo.id}>
+            <span>{todo.completed.toString()+' '}</span>
+            <label>{todo.title+' '}</label>
+            <button onClick={updateTodo.bind(that, todo.id)}>
+              {'Update'}
+            </button>
+            <button onClick={deleteTodo.bind(that, todo.id)}>
+              {'X'}
+            </button>
+          </div>
+        )
+      }
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/constants/index.js
@@ -0,0 +1,5 @@
+export const API_URL = 'http://localhost:8000/api/v1/todos/';
+export const SLASH = '/';
+export const TODO_TITLE = 'title';
+export const TODO_ID = 'id';
+export const TODO_COMPLETED = 'completed';
