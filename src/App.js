b61232c1414edd9cc0d93e24c034604978b46b91
feelextra <felxtra@gmail.com>
2018-10-24 18:09:15 +0300

    finished todoController with feature: TodoItem components have a loading prop depending on if server responded yet or not

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { ESCAPE_KEY, ENTER_KEY } from "./constants/index.js";
+import { ENTER_KEY } from "./constants/index.js";
+import TodoItem from "./TodoItem.js";
 
 class App extends Component {
   constructor(props) {
@@ -91,87 +92,15 @@ const TodoList = ({
           key={todo.id}
           controller={controller}
           todo={todo}
-          editing={editingId === todo.id}
+          isEditing={editingId === todo.id}
           onSave={(...args) => onSave(...args)}
           onEdit={() => onEdit(todo)}
           onCancel={() => onCancel()}
+          isLoading={controller.loading.includes(todo.id)}
         />
       ))}
     </div>
   );
 };
-
-class TodoItem extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      editText: ""
-    };
-  }
-
-  handleEditTodoTextChange(event) {
-    this.setState({ editText: event.target.value });
-  }
-
-  handleEditTodoKeyDown(event) {
-    const pressedEnter = event.keyCode === ENTER_KEY;
-    if (!pressedEnter) {
-      return;
-    }
-
-    if (event.which === ESCAPE_KEY) {
-      this.setState({ editText: this.props.todo.title });
-      this.props.onCancel();
-    } else if (event.which === ENTER_KEY) {
-      this.handleSubmit();
-    }
-  }
-
-  handleSubmit() {
-    const editTextValue = this.state.editText.trim();
-    if (editTextValue.length > 0) {
-      this.props.onSave(this.props.todo, editTextValue);
-      this.setState({ editText: "" });
-    }
-  }
-
-  deleteTodo() {
-    this.props.controller.deleteTodo(this.props.todo.id);
-  }
-
-  handleEdit() {
-    this.props.onEdit();
-  }
-
-  render() {
-    const { todo, editing } = this.props;
-    const { editText } = this.state;
-    if (todo.loading) {
-      return <span key={todo.id}>{"Loading..."}</span>;
-    }
-    if (editing) {
-      return (
-        <div key={todo.id} className="edit-todo-field">
-          <input
-            value={editText}
-            onChange={event => this.handleEditTodoTextChange(event)}
-            onKeyDown={event => this.handleEditTodoKeyDown(event)}
-            type="text"
-            placeholder="Edited value for todo"
-          />
-        </div>
-      );
-    }
-    return (
-      <div key={todo.id}>
-        <span>{todo.completed.toString() + " "}</span>
-        <label onDoubleClick={() => this.handleEdit()}>
-          {todo.title + " "}
-        </label>
-        <button onClick={() => this.deleteTodo()}>{"X"}</button>
-      </div>
-    );
-  }
-}
 
 export default App;
