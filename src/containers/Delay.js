caa82d15c8e5027c14705350b60779ef9454f624
feelextra <felxtra@gmail.com>
2018-12-11 03:20:24 +0200

    Delay component for loading auth

---
+++ src/containers/Delay.js
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+
+const Loader = styled.div`
+  height: 10px;
+  width: 100%;
+  background-color: hotpink;
+`;
+
+class Delay extends React.Component {
+  state = { timeoutOver: false };
+
+  componentDidMount() {
+    window.setTimeout(() => {
+      this.setState({ timeoutOver: true });
+    }, this.props.timeout);
+  }
+
+  render() {
+    const { component: Component, shouldShow, timeout, ...rest } = this.props;
+    if (shouldShow) return <Component {...rest} />;
+    else if (this.state.timeoutOver) return <Loader timeout={timeout} />;
+    else return null;
+  }
+}
+
+export default Delay;
--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -3,20 +3,25 @@ import React from "react";
 import Header from "./Header.js";
 import Body from "./Body/index.js";
 import AuthContext from "../containers/Auth/AuthContext.js";
+import Delay from "../containers/Delay.js";
 
 const Layout = () => (
   <AuthContext.Consumer>
-    {authContext =>
-      authContext.isAwaitingAuth ? (
-        <h1>Loading Page</h1>
-      ) : (
-        <div>
-          <Header />
-          <Body />
-        </div>
-      )
-    }
+    {authContext => (
+      <Delay
+        component={ScreenLayout}
+        shouldShow={!authContext.isAwaitingAuth}
+        timeout={500}
+      />
+    )}
   </AuthContext.Consumer>
+);
+
+const ScreenLayout = () => (
+  <div>
+    <Header />
+    <Body />
+  </div>
 );
 
 export default Layout;
