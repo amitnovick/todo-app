2367000587df34f1259d370efa422cc191d22ff3
feelextra <felxtra@gmail.com>
2019-04-27 22:42:55 +0300

    Replaced styled-components with css-modules for one component

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -2,14 +2,6 @@ import React from 'react';
 
 import styles from './style.module.css';
 
-import {
-  StyledInputEdit,
-  StyledInputCheckbox,
-  StyledLabelTitle,
-  StyledUnorderedList,
-  StyledDiv,
-} from './style.js';
-
 const ENTER_KEY = 13;
 const ESCAPE_KEY = 27;
 
@@ -83,15 +75,16 @@ class TodoList extends React.Component {
     const { editTitle } = this.state;
     console.log('styles?', styles);
     return (
-      <StyledDiv>
-        <StyledUnorderedList>
+      <div className={styles['div-1']}>
+        <ul className={styles['ul-1']}>
           {todos.map(todo => {
             const isBeingEdited = this.isTodoBeingEdited(todo);
             let content;
             if (isBeingEdited) {
               content = (
                 <div key={todo.id}>
-                  <StyledInputEdit
+                  <input
+                    className={styles['input-2']}
                     ref={this.inputRef}
                     value={editTitle}
                     onChange={event => this.handleEditTitleTextChange(event)}
@@ -106,17 +99,27 @@ class TodoList extends React.Component {
             } else {
               content = (
                 <div>
-                  <StyledInputCheckbox
+                  <input
+                    className={styles['input-1']}
                     type="checkbox"
                     checked={todo.completed}
                     onChange={() => onToggle(todo)}
                   />
-                  <StyledLabelTitle
-                    isCompleted={todo.completed}
+                  <label
+                    className={styles['label-1']}
+                    style={
+                      todo.completed
+                        ? {
+                            color: '#d9d9d9',
+                            textDecoration: 'line-through',
+                            backgroundImage: `url('data:image/svg+xml;utf8,%3Csvg%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20width%3D%2240%22%20height%3D%2240%22%20viewBox%3D%22-10%20-18%20100%20135%22%3E%3Ccircle%20cx%3D%2250%22%20cy%3D%2250%22%20r%3D%2250%22%20fill%3D%22none%22%20stroke%3D%22%23bddad5%22%20stroke-width%3D%223%22/%3E%3Cpath%20fill%3D%22%235dc2af%22%20d%3D%22M72%2025L42%2071%2027%2056l-4%204%2020%2020%2034-52z%22/%3E%3C/svg%3E')`,
+                          }
+                        : {}
+                    }
                     onDoubleClick={() => this.handleTitleClick(todo)}
                   >
                     {todo.title + ' '}
-                  </StyledLabelTitle>
+                  </label>
                   <button
                     className={styles['button-1']}
                     onClick={() => onDelete(todo)}
@@ -130,8 +133,8 @@ class TodoList extends React.Component {
               </li>
             );
           })}
-        </StyledUnorderedList>
-      </StyledDiv>
+        </ul>
+      </div>
     );
   }
 }
