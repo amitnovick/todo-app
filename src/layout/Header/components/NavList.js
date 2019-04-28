d6ee8180e3744467c0654e5073245111df480cb9
feelextra <felxtra@gmail.com>
2019-04-28 18:37:50 +0300

    Refactored NavBar, NavList components

--- src/layout/Header/components/NavList.js
+++ src/layout/Header/components/NavList.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Nav, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-import withAuthentication from '../containers/Auth/withAuthentication.js';
-import ModalWithActivator from './ModalWithActivator.js';
+import withAuthentication from '../../../containers/Auth/withAuthentication.js';
+import ModalWithActivator from '../../../components/ModalWithActivator.js';
 
 const NavList = ({ isAuthenticated }) =>
   isAuthenticated ? (
@@ -25,22 +25,22 @@ const NavList = ({ isAuthenticated }) =>
       </NavItem>
     </Nav>
   ) : (
-    <Nav className="ml-auto" navbar>
-      <NavItem />
-      <NavItem>
-        <NavLink tag={Link} to="/demo">
-          Demo
+      <Nav className="ml-auto" navbar>
+        <NavItem />
+        <NavItem>
+          <NavLink tag={Link} to="/demo">
+            Demo
         </NavLink>
-      </NavItem>
-      <NavItem>
-        <NavLink tag={Link} to="/features">
-          Features
+        </NavItem>
+        <NavItem>
+          <NavLink tag={Link} to="/features">
+            Features
         </NavLink>
-      </NavItem>
-      <NavItem>
-        <ModalWithActivator />
-      </NavItem>
-    </Nav>
-  );
+        </NavItem>
+        <NavItem>
+          <ModalWithActivator />
+        </NavItem>
+      </Nav>
+    );
 
 export default withAuthentication(NavList);
