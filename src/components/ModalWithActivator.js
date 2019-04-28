8636ae3440d8053d8dd78b182d52c12eca7bb5b5
feelextra <felxtra@gmail.com>
2019-04-28 18:40:35 +0300

    Refactored LoginModal

--- src/components/ModalWithActivator.js
+++ src/components/ModalWithActivator.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button } from "reactstrap";
 
-import LoginModal from "./LoginModal.js";
+import LoginModal from "../layout/Header/components/LoginModal.js";
 
 class ModalWithActivator extends React.Component {
   constructor(props) {
