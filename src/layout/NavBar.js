3d73f2d568fb99686fbd2b445c0a839b04ed098f
feelextra <felxtra@gmail.com>
2019-05-28 00:54:05 +0300

    Refactored logic a bit

--- src/layout/NavBar.js
+++ src/layout/NavBar.js
@@ -6,6 +6,89 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 import cloudLogo from '../assets/cloud.png';
 
+const BrandMenuItem = ({ handleItemClick }) => (
+  <Menu.Item
+    as={Link}
+    to={'/'}
+    active={false}
+    name="Home"
+    onClick={handleItemClick}
+  >
+    <img src={cloudLogo} alt="cloud_logo" />
+  </Menu.Item>
+);
+
+const NavigationMenuItems = ({ items, activeItem, handleItemClick }) =>
+  items.map(({ path, menuTitle }) => (
+    <Menu.Item
+      as={Link}
+      to={path}
+      key={path}
+      active={activeItem === path}
+      name={path}
+      onClick={handleItemClick}
+    >
+      {menuTitle}
+    </Menu.Item>
+  ));
+
+const BiggerScreens = ({ items, activeItem, handleItemClick }) => (
+  <Grid.Row columns={1} only="tablet computer">
+    <Grid.Column>
+      <Menu color="teal" inverted size="huge">
+        <BrandMenuItem handleItemClick={handleItemClick} />
+        <Menu.Menu position="right">
+          <NavigationMenuItems
+            items={items}
+            activeItem={activeItem}
+            handleItemClick={handleItemClick}
+          />
+        </Menu.Menu>
+      </Menu>
+    </Grid.Column>
+  </Grid.Row>
+);
+
+const SmallerScreens = ({
+  items,
+  activeItem,
+  handleItemClick,
+  isCollapsed,
+  setIsCollapsed
+}) => (
+  <React.Fragment>
+    <Grid.Row columns={1} only="mobile" style={{ paddingBottom: 0 }}>
+      <Grid.Column>
+        <Menu color="teal" inverted size="huge">
+          <BrandMenuItem handleItemClick={handleItemClick} />
+          <Menu.Menu position="right">
+            <Menu.Item>
+              <FontAwesomeIcon
+                icon={faBars}
+                style={{ cursor: 'pointer' }}
+                onClick={() => setIsCollapsed(!isCollapsed)}
+              />
+            </Menu.Item>
+          </Menu.Menu>
+        </Menu>
+      </Grid.Column>
+    </Grid.Row>
+    <Grid.Row columns={1} only="mobile" style={{ paddingTop: 0 }}>
+      <Grid.Column>
+        {isCollapsed ? null : (
+          <Menu color="teal" inverted size="huge" stackable>
+            <NavigationMenuItems
+              items={items}
+              activeItem={activeItem}
+              handleItemClick={handleItemClick}
+            />
+          </Menu>
+        )}
+      </Grid.Column>
+    </Grid.Row>
+  </React.Fragment>
+);
+
 const MenuExampleStackable = ({ items }) => {
   const [activeItem, setActiveItem] = React.useState(null);
   const [isCollapsed, setIsCollapsed] = React.useState(true);
@@ -14,79 +97,18 @@ const MenuExampleStackable = ({ items }) => {
 
   return (
     <Grid style={{ marginBottom: 10 }}>
-      <Grid.Row columns={1} only="tablet computer">
-        <Grid.Column>
-          <Menu color="teal" inverted size="huge">
-            <Menu.Item
-              as={Link}
-              to={'/'}
-              active={false}
-              name="Home"
-              onClick={handleItemClick}
-            >
-              <img src={cloudLogo} alt="cloud_logo" />
-            </Menu.Item>
-            <Menu.Menu position="right">
-              {items.map(({ path, menuTitle }) => (
-                <Menu.Item
-                  as={Link}
-                  to={path}
-                  key={path}
-                  active={activeItem === path}
-                  name={path}
-                  onClick={handleItemClick}
-                >
-                  {menuTitle}
-                </Menu.Item>
-              ))}
-            </Menu.Menu>
-          </Menu>
-        </Grid.Column>
-      </Grid.Row>
-      <Grid.Row columns={1} only="mobile" style={{ paddingBottom: 0 }}>
-        <Grid.Column>
-          <Menu color="teal" inverted size="huge">
-            <Menu.Item
-              as={Link}
-              to={'/'}
-              active={false}
-              name="Home"
-              onClick={handleItemClick}
-            >
-              <img src={cloudLogo} alt="cloud_logo" />
-            </Menu.Item>
-            <Menu.Menu position="right">
-              <Menu.Item>
-                <FontAwesomeIcon
-                  icon={faBars}
-                  style={{ cursor: 'pointer' }}
-                  onClick={() => setIsCollapsed(!isCollapsed)}
-                />
-              </Menu.Item>
-            </Menu.Menu>
-          </Menu>
-        </Grid.Column>
-      </Grid.Row>
-      <Grid.Row columns={1} only="mobile" style={{ paddingTop: 0 }}>
-        <Grid.Column>
-          {isCollapsed ? null : (
-            <Menu color="teal" inverted size="huge" stackable>
-              {items.map(({ path, menuTitle }) => (
-                <Menu.Item
-                  as={Link}
-                  to={path}
-                  key={path}
-                  active={activeItem === path}
-                  name={path}
-                  onClick={handleItemClick}
-                >
-                  {menuTitle}
-                </Menu.Item>
-              ))}
-            </Menu>
-          )}
-        </Grid.Column>
-      </Grid.Row>
+      <BiggerScreens
+        items={items}
+        activeItem={activeItem}
+        handleItemClick={handleItemClick}
+      />
+      <SmallerScreens
+        items={items}
+        activeItem={activeItem}
+        handleItemClick={handleItemClick}
+        isCollapsed={isCollapsed}
+        setIsCollapsed={setIsCollapsed}
+      />
     </Grid>
   );
 };
