4ce4d17ebfea4ff8f9c069498e69b5446912f222
feelextra <felxtra@gmail.com>
2019-05-28 23:44:27 +0300

    Implemented home page design

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,85 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const HomeScreen = () => <h1>Nothing much here yet...</h1>;
+import sharedRoutes from '../routes/sharedRoutes';
+import clipboardLogo from '../assets/Clipboard-check.svg';
+
+const semanticUiTeal = '#008080';
+
+const buttonHoverPopAnimation = {
+  transform: 'translateY(-0.25em)',
+  '-webkit-box-shadow': '0px 5px 40px -10px rgba(0,0,0,0.57)'
+};
+
+const HomeScreen = () => (
+  <div style={{ textAlign: 'center' }}>
+    <img src={clipboardLogo} alt="clipboard_logo" />
+    <h2
+      style={{
+        fontFamily: "'Roboto', sans-serif",
+        fontWeight: 600,
+        textTransform: 'uppercase',
+        letterSpacing: '0.2em'
+      }}
+    >
+      Get Things Done
+    </h2>
+    <h2 style={{ fontFamily: "'Roboto', sans-serif", fontWeight: 600 }}>
+      One step at a time
+    </h2>
+    <h3
+      style={{
+        fontFamily: "'Roboto', sans-serif",
+        fontWeight: 400,
+        fontSize: '1.3em'
+      }}
+    >
+      Notes are stored on the cloud and go wherever you go.
+    </h3>
+    <Link
+      to={sharedRoutes.APP}
+      color="teal"
+      css={{
+        backgroundColor: semanticUiTeal,
+        fontSize: '1em',
+        borderRadius: '0.5em',
+        border: '1px solid',
+        padding: '16px 24px',
+        color: 'white',
+        ':after': { content: "'▶'", paddingLeft: '0.5em' },
+        ':hover': {
+          ...buttonHoverPopAnimation,
+          color: 'white'
+        },
+        margin: '1em 1em 1em 0',
+        display: 'inline-block'
+      }}
+    >
+      GET STARTED
+    </Link>
+    <Link
+      to={sharedRoutes.FEATURES}
+      color="teal"
+      css={{
+        backgroundColor: 'white',
+        color: semanticUiTeal,
+        fontSize: '1em',
+        borderRadius: '0.5em',
+        border: '1px solid',
+        padding: '16px 24px',
+        ':after': { content: "'▶'", paddingLeft: '0.5em' },
+        margin: '1em 1em 1em 0',
+        display: 'inline-block',
+        ':hover': {
+          ...buttonHoverPopAnimation
+        }
+      }}
+    >
+      LEARN MORE
+    </Link>
+  </div>
+);
 
 export default HomeScreen;
