3b24075f5266b97b7a163d793fc8fb9371fb04ea
feelextra <felxtra@gmail.com>
2019-05-30 23:07:57 +0300

    1. Removed module.hot.accept() 2. Removed normalize.css

--- src/index.js
+++ src/index.js
@@ -3,15 +3,10 @@ import ReactDOM from 'react-dom';
 
 import * as serviceWorker from './serviceWorker';
 import ScreenRouter from './ScreenRouter';
-import 'normalize.css';
 import './global.css';
 import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(<ScreenRouter />, document.getElementById('root'));
-
-if (module.hot) {
-  module.hot.accept();
-}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
